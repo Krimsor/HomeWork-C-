3fc35c6ae755a5cebaac770fe4583a2220e365b4
Vadim <vsuffering@gmail.com>
2022-10-02 14:41:10 +1000

    add home work 002

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача №4 : Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+/*int a = 2, b = 3, c = 7;
+int max = c;
+Console.WriteLine(max = c);
+
+int a = 44, b = 5, c = 78;
+int max = c;
+Console.WriteLine(max = c);
+
+int a = 22, b = 3, c = 9;
+int max = a;
+Console.WriteLine(max = a);*/
