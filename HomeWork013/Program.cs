3195cf35be6e4a8f77b6fdd72ed4fae601ad0dd7
Vadim <vsuffering@gmail.com>
2022-10-10 22:48:23 +1000

    add  HW 013

---
+++ HomeWork013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+int[] array = CreateArray();
+
+foreach (var item in array)
+    Console.Write($"{item} ");
+
+int[] CreateArray()
+{
+    int[] _array = new int[8];
+    for (int i = 0; i < _array.Length; i++)
+        _array[i] = new Random().Next(0, 100);
+    return _array;
+}
