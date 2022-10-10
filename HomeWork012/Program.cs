942a030a5f852bcf313d7738603fe71edc27781c
Vadim <vsuffering@gmail.com>
2022-10-10 17:26:23 +1000

    add  HW 012

---
+++ HomeWork012/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write($"Сумма цифр в числе {a} = {Summa(a)}");
+
+int Summa(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum = sum + a %  10;
+        a /= 10;
+    }
+    return sum;       
+}
+
