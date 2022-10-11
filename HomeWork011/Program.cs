0537217a89ef299daa421868ef811f01caafd410
Vadim <vsuffering@gmail.com>
2022-10-11 18:37:09 +1000

    change  HW 011

--- HomeWork011/Program.cs
+++ HomeWork011/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
-int A, B;
+/*int A, B;
 Console.WriteLine($"Введите число: ");
 int.TryParse(Console.ReadLine()!, out A);
 int.TryParse(Console.ReadLine()!, out B);
@@ -9,4 +9,12 @@ for (int i = 1; i < B; i++)
 {
     exp = exp * A;
 }
-Console.WriteLine($"{A}^{B}-> {exp}");
+Console.WriteLine($"{A}^{B} -> {exp}");*/
+
+double A, B;
+Console.WriteLine($"Введите число: ");
+double.TryParse(Console.ReadLine()!, out A);
+double.TryParse(Console.ReadLine()!, out B);
+
+double exp = Math.Pow( A, B );
+Console.WriteLine($"{A}^{B} -> {exp}");
