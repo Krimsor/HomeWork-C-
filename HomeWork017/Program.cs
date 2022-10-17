21aaa6188af28e020feda2e978b3cd448b57da61
Vadim <vsuffering@gmail.com>
2022-10-17 19:55:59 +1000

    add HW 017

---
+++ HomeWork017/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите количество элементов массива: ");
+
+int elementscount = int.Parse(Console.ReadLine()!);
+
+int [] numbers = new int [elementscount];
+
+Console.WriteLine($"Ввод массива: ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine()!); 
+}
+
+int result = 0;
+
+Console.WriteLine ($"Вывод массива: ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($" {numbers[i]} ");
+
+    if (numbers[i] > 0)
+    {
+        result++;
+    }
+}
+Console.Write($"-> {result}");
