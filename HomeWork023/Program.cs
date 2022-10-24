5e673338efdb3dedc15f3b5af0900fefb5ae72ec
Vadim <vsuffering@gmail.com>
2022-10-24 14:22:57 +1000

    add HW 023

---
+++ HomeWork023/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine($"Введите размер массива: ");
+int a;
+int.TryParse(Console.ReadLine()!, out a);
+int b;
+int.TryParse(Console.ReadLine()!, out b);
+
+int[,] array = new int[a, b];
+
+FillMatrix(array);
+PrintArray(array);
+Console.WriteLine();
+NumberRowMinSumElements(array);
+
+void NumberRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+
+                sumRow += array[i, j];
+            if (sumRow < minRow)
+            {
+                minRow = sumRow;
+                minSumRow = i;
+            }
+            sumRow = 0;
+        }
+        Console.Write($"{minSumRow + 1} строка");
+    }
+}
+
+void FillMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
