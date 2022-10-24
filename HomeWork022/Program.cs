c416ab43a149c8300bb370ef6af2956759eb4b8f
Vadim <vsuffering@gmail.com>
2022-10-24 13:56:37 +1000

    add HW 022

---
+++ HomeWork022/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine($"Введите размер массива: ");
+int a;
+int.TryParse(Console.ReadLine()!, out a);
+int b;
+int.TryParse(Console.ReadLine()!, out b);
+
+int[,] array = new int[a, b];
+
+
+FillMatrix(array);
+PrintArray(array);
+SortToLower(array);
+Console.WriteLine();
+PrintArray(array);
+
+void FillMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortToLower(int[,] array)// сортировка по убыванию
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
