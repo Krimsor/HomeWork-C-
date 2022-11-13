e4b7bb9d744d955fcd11e3c1ddb1d7acd487a0ac
Vadim <vsuffering@gmail.com>
2022-11-13 14:53:10 +1000

    add HW 027

---
+++ HomeWork027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine($"Введите M и N: ");
+int M;
+int.TryParse(Console.ReadLine()!, out M);
+int N;
+int.TryParse(Console.ReadLine()!, out N);
+
+void SumNatNumbers(int numM, int numN, int sum)
+{
+    if (numM > numN)
+    {
+        Console.WriteLine($"{M} {N} -> {sum}");
+        return;
+    }
+    sum = sum + (numM++);
+    SumNatNumbers(numM, numN, sum);
+}
+
+SumNatNumbers(M, N, 0);
